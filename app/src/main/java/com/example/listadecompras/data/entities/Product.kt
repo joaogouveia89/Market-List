7a630c715a7828eee56799837814d69d1d36beff
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-11-02 15:29:00 +0000

    prepared code to parcelable

--- app/src/main/java/com/example/listadecompras/data/entities/Product.kt
+++ app/src/main/java/com/example/listadecompras/data/entities/Product.kt
@@ -1,16 +1,19 @@
 package com.example.listadecompras.data.entities
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(
     indices = [Index(value = ["name"], unique = true)]
 )
 data class Product(@PrimaryKey(autoGenerate = true) val uid: Int = 0,
                    @ColumnInfo(name = "name") val name: String?,
                    @ColumnInfo(name = "quantity") val quantity: Int = 1,
-                   @ColumnInfo(name = "price") val price: Double = 0.0){
+                   @ColumnInfo(name = "price") val price: Double = 0.0): Parcelable{
     fun totalCost() = price * quantity
 }
